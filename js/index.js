ef40341f15d9a7cdd4efc2fb2d75280e0515b653
piry9200 <ymt104ro09@gmail.com>
2023-01-19 21:37:43 +0900

    add: 基本的なポモドーロタイマーが完成 あとはリセット機能などを追加したい

--- js/index.js
+++ js/index.js
@@ -1,132 +1,148 @@
-const form_minute = document.getElementById("form_minute");
-const form_second = document.getElementById("form_second");
-const start_button = document.getElementById("start_button");
-const stop_button = document.getElementById("stop_button");
+const work_form_minute = document.getElementById("work_form_minute");
+const work_form_second = document.getElementById("work_form_second");
+const rest_form_minute = document.getElementById("rest_form_minute");
+const rest_form_second = document.getElementById("rest_form_second");
+const start_stop_button = document.getElementById("start_stop_button");
 const reset_button = document.getElementById("reset_button");
-const minute_part =  document.getElementById("minute");
-const second_part =  document.getElementById("second");
+const display_minute =  document.getElementById("display_minute");
+const display_second =  document.getElementById("display_second");
+const display_status =  document.getElementById("display_status");
+const apply_form_buttons = document.getElementsByClassName("apply_form");
 
 class Timer {
-    constructor(min, sec){
+    constructor(min, sec, status, timerId, posing, must_change){
         this.min = min;
         this.sec = sec;
+        this.status = status; //集中: status > 0, 休憩: status < 0
+        this.timerId = timerId;
+        this.posing = posing; //カウントダウン中はtrue
+        this.must_change = must_change
     }
 
-    countdown_Min(){
-        this.min = this.min - 1;
-    }
-
-    countdown_Sec(){
-        if(this.sec != 0){
+    countdown(){ //00:00になるまでカウントダウンする。00:00になったらmust_changeをtrueにして、Intervalを止め、statusを0にする.
+        if(this.sec != 0){ //秒が0でないときに実行
             this.sec = this.sec - 1;
-        }else{
-            this.sec = 59;
+        }else{ //秒が0のときに実行
+            if(this.min == 0){//00:00のときの処理
+                if(timer.status == 10){ //次のモード(休憩モード)の値をフォームから取得し表示を変える
+                    timer.status = -10; //休憩モードの時間を取得するためにstatusを-10にする
+                    confirm_form();
+                    display_status.textContent = "休憩";
+                    display_minute.textContent = timer.min;
+                    display_second.textContent = timer.sec;
+                    start_stop_button.textContent = "スタート";
+                    console.log("休憩モードにする");
+                }else if(timer.status == -10){ //次のモード(集中モード)の値をフォームから取得し表示を変える
+                    timer.status = 10; //集中モードの時間を取得するためにstatusを10にする
+                    confirm_form();
+                    display_status.textContent = "集中";
+                    display_minute.textContent = timer.min;
+                    display_second.textContent = timer.sec;
+                    start_stop_button.textContent = "スタート";
+                    console.log("集中モードにする");
+                }
+                this.must_change = true;
+                clearInterval(this.timerId);
+            }else{
+                this.min = this.min - 1;
+                this.sec = 59;
+            }
         }
     }
 }
 
-let timerId = null; //カウントダウン処理のインターバル関数ID用のグローバル関数
-let is_countdowning = false;
-const init_min = Number(form_minute.value)
-const init_sec = Number(form_second.value)
-const timer = new Timer(init_min, init_sec);
-
-function render_time(){ //引数に書き換えるDOM要素を渡す
+function render_time(){
         const timebox = document.getElementById("time");
         let date = new Date(); //Dateオブジェクトのインスタンスを作った時点での日時を取得できるのでインスタンス作成もIntervalに入れる
         let hour = date.getHours();
         let minute = date.getMinutes();
         let second = date.getSeconds();
         timebox.textContent = hour + ":" + minute + ":" + second;
+        console.log("timer.status: " + timer.status);
+        console.log("timer.posing: " + timer.posing);
+        console.log("timer.must_change: " + timer.must_change);
+        console.log("");
 }
 
-function update_minute(){ //表示する分をフォームに合わせる
-    if(!is_countdowning){
-        minute_part.textContent = Number(form_minute.value);
+function confirm_form(){ //timerのメンバ変数をフォームに合わせる
+    if(timer.status == 10){//集中モード中の時は「集中」のフォームから値を取得
+        timer.min = Number(work_form_minute.value);
+        timer.sec = Number(work_form_second.value);
+    }else if(timer.status == -10){//休憩モード中の時は「休憩」のフォームから値を取得
+        timer.min = Number(rest_form_minute.value);
+        timer.sec = Number(rest_form_second.value);
     }
+    console.log("confirm");
 }
 
-function update_second(){ //表示する秒をフォームに合わせる
-    if(!is_countdowning){
-        second_part.textContent = Number(form_second.value);
+function handleTimer(){
+    if((timer.status == 10 && !timer.posing && !timer.must_change) || (timer.status == -10 && !timer.posing && !timer.must_change)){ //集中モード中 休憩モード中
+        timer.countdown();
+        //タイマー表示を書き換える
+        display_minute.textContent = timer.min;
+        display_second.textContent = timer.sec;
+        console.log("min:sec " + timer.min + ":" + timer.sec);
     }
-}
-function countdown(){
-    is_countdowning = true;
-    timerId = setInterval(() => {
-        if(timer.sec != 0){
-            timer.countdown_Sec()
-            if(timer.sec < 10){
-                second_part.textContent = "0" + timer.sec;
-            } else{
-                second_part.textContent = timer.sec;
-            }
-            console.log(timer.min + " " + timer.sec);
-        }else{
-            if(timer.min == 0){
-                clearInterval(timerId);
-                is_countdowning = false;
-            }else{
-                timer.countdown_Min();
-                timer.countdown_Sec();
-                if(timer.min > 10){
-                    minute_part.textContent = timer.min;
-                    second_part.textContent = timer.sec;
-                }else{
-                    minute_part.textContent = "0" + timer.min;
-                    second_part.textContent = timer.sec;
-                }
-                
-            }
-        }
-        console.log("Interval");
-        console.log(timer.sec);
-    },1000)
-};
+    //console.log("handletimer");
 
-function handle_clicked_start_button(){
-    if(!is_countdowning){
-        timer.min = Number(form_minute.value);
-        timer.sec = Number(form_second.value);
-        countdown(timer);
-    }
 }
 
-function handle_clicked_stop_button(){
-    if(timerId != null){
-        clearInterval(timerId);
-        is_countdowning = false;
-        timerId = null;
-        console.log(timer);
-        console.log(timer.min + " test " + timer.sec);
+function handle_clicked_start_stop_button(){
+    if(timer.status == 10 && !timer.posing && timer.must_change){ //集中開始前に押された場合
+        timer.status = 10; //statusを集中モードに
+        timer.must_change = false;
+        confirm_form(); //timerにformの時間をセット
+        timer.timerId = setInterval("handleTimer()", 1000);
+        start_stop_button.textContent = "ストップ";
+    }else if(timer.status == 10 && !timer.posing && !timer.must_change){ //集中モード中に押された場合
+        clearInterval(timer.timerId);
+        timer.posing = true;
+        start_stop_button.textContent = "スタート";
+    }else if(timer.status == 10 && timer.posing && !timer.must_change){ //集中モード中の一時停止中に押された場合
+        timer.posing = false;
+        timer.timerId = setInterval("handleTimer()", 1000);
+        start_stop_button.textContent = "ストップ";
+    }else if(timer.status == -10 && !timer.posing && timer.must_change){ //休憩モード開始前に押された場合
+        timer.status = -10; //statusを休憩モードに
+        timer.must_change = false;
+        confirm_form(); //timerにformの時間をセット
+        timer.timerId = setInterval("handleTimer()", 1000);
+        start_stop_button.textContent = "ストップ";
+    }else if(timer.status == -10 && !timer.posing && !timer.must_change){ //休憩モード中に押された場合
+        clearInterval(timer.timerId);
+        timer.posing = true;
+        start_stop_button.textContent = "スタート";
+    }else if(timer.status == -10 && timer.posing && !timer.must_change){ //休憩モード中の一時停止中に押された場合
+        timer.posing = false;
+        timer.timerId = setInterval("handleTimer()", 1000);
+        start_stop_button.textContent = "ストップ";
     }
-    else{
-        countdown(timer);    
-    }
-    console.log(timerId);
 }
 
 function handle_clicked_reset_button(){
-    if(timerId != null){
-        clearInterval(timerId);
-        is_countdowning = false;
-        timerId = null;
-        timer.min = Number(form_minute.valu);
-        timer.sec = Number(form_second.value);
-        minute_part.textContent = form_minute.value;
-        second_part.textContent = form_second.value;
-    }else{
-        timer.min = Number(form_minute.valu);
-        timer.sec = Number(form_second.value);
-        minute_part.textContent = form_minute.value;
-        second_part.textContent = form_second.value;
+    
+}
+
+function handle_clicked_apply_button(){ // 集中モード、休憩モードの開始時または、一時停止中に実行できる
+    if( (timer.status == 10 && !timer.posing && timer.must_change) || (timer.status == -10 && !timer.posing && timer.must_change) || (timer.status == 10 && timer.posing && !timer.must_change) || (timer.status == -10 && timer.posing && !timer.must_change) ){
+        confirm_form();
+        display_minute.textContent = timer.min;
+        display_second.textContent = timer.sec;
     }
 }
 
-form_minute.addEventListener("input", update_minute);
-form_second.addEventListener("input", update_second);
-start_button.addEventListener("click", handle_clicked_start_button);
-stop_button.addEventListener("click", handle_clicked_stop_button);
+start_stop_button.addEventListener("click", handle_clicked_start_stop_button);
 reset_button.addEventListener("click", handle_clicked_reset_button);
+for(let i=0; i<2; i++){
+    apply_form_buttons[i].addEventListener("click", handle_clicked_apply_button);
+}
+
+// timerの初期設定
+const init_min = Number(work_form_minute.value)
+const init_sec = Number(work_form_second.value)
+const timer = new Timer(init_min, init_sec, 10, null, false, true);
+//タイマー表示の初期設定
+display_minute.textContent = timer.min;
+display_second.textContent = timer.sec;
 
 setInterval("render_time()", 1000);
