b39eafef74155c9cb7cc679659f870532ef05a51
piry9200 <ymt104ro09@gmail.com>
2023-01-20 12:57:37 +0900

    update: デザインを仮で整えた

--- js/get_cats.js
+++ js/get_cats.js
@@ -1,5 +1,6 @@
 const img_cats = document.getElementById("img_cats");
 const neko_button = document.getElementById("neko_button");
+const neko_change_button = document.getElementById("neko_change_button");
 
 async function get_cats(){
     console.log("neko");
@@ -21,14 +22,17 @@ function control_neko(event){
     console.log(event);
     if(event.target.textContent == "猫非表示"){
         img_cats.style.display = "none";
+        neko_change_button.style.display = "none";
         event.target.textContent = "猫表示"
     } else if(event.target.textContent == "猫表示"){
         img_cats.style.display = "block";
+        neko_change_button.style.display = "inline";
         event.target.textContent = "猫非表示"
     }
 }
 
 neko_button.addEventListener("click", control_neko);
+neko_change_button.addEventListener("click", get_cats);
 
 get_cats(); //初期表示
 setInterval("get_cats()", 60000 * 10);
